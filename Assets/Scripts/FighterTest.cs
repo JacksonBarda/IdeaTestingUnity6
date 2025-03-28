c9ec1954a887da341a83ddd8eb27bcf6e7fc02c5
JacksonBarda <43941043+JacksonBarda@users.noreply.github.com>
2025-03-27 22:09:53 -0400

    Add Circle prefab meta file, update Visual Studio configuration, and enhance FighterTest script
    - Added a new meta file for Circle prefab to manage its settings.
    - Updated the Visual Studio package version from 2.0.22 to 2.0.23 in manifest.json.
    - Enhanced FighterTest script by adding TextMeshProUGUI references for attack and defense UI, initializing their values in Start method, and implementing logic for the NextTurn method to update defense status.

--- Assets/Scripts/FighterTest.cs
+++ Assets/Scripts/FighterTest.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using TMPro; // Make sure to include this namespace for TextMeshPro
 
 public class FighterTest : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI attackTextUI; // Reference to the TextMeshProUGUI component
+    [SerializeField]
+    private int attackPower = 10;
+    [SerializeField]
+    private TextMeshProUGUI defenseTextUI; // Reference to the TextMeshProUGUI component
+    [SerializeField]
+    private int defensePower = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        attackTextUI.text = attackPower.ToString();
+        defenseTextUI.text = defensePower.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void NextTurn(){
+        if(attackPower > 0)
+        {
+            //attackPower--;
+            //attackTextUI.text = attackPower.ToString();
+        }
+        if(defensePower > 0){
+            defensePower--;
+            defenseTextUI.text = defensePower.ToString();
+        }else{
+            attackTextUI.text = "";
+            defenseTextUI.text = "dead";
+        }
     }
 }
